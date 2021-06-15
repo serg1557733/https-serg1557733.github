b0d82ac57ca1e427295f647076892ab4e43bafdf
serg1557733 <serg1557733@ukr.net>
2021-06-15 22:12:35 +0300

    portfolio finish

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,4 @@
+//hamburger menu activation classes
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const closeElem = document.querySelector('.menu__close');
@@ -9,4 +10,22 @@ hamburger.addEventListener('click',() => {
 closeElem.addEventListener('click',() => {
     menu.classList.remove('active')
     hamburger.classList.remove('hamburger_active')
+});
+
+//auto progress bar width 
+const skillsCounter = document.querySelectorAll('.skills__counter');
+const skillsBar = document.querySelectorAll('.skills__progress-status span');
+
+skillsCounter.forEach((item, i) => {
+    skillsBar[i].style.width = item.textContent;
+});
+
+//slow-scroll script 
+$(document).ready(function(){
+    $("#btns").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
 });
